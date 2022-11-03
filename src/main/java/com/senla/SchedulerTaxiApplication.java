36577f9ea58cc5722a996f28bea561fc6a8bd0e1
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-11-03 12:15:49 +0300

    Initial commit

---
+++ src/main/java/com/senla/SchedulerTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.senla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerTaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senla/SchedulerTaxiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.senla;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerTaxiApplicationTests {
+
+
+
+}
